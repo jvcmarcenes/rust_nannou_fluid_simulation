78f57a21fe836299e023a52f25cbeebb68ccf559
Joao <jvcmv.jvcmarcenes@gmail.com>
2022-07-05 11:12:15 -0300

    added multithreading

--- src/fluid.rs
+++ src/fluid.rs
@@ -1,25 +1,25 @@
-use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator, IndexedParallelIterator, IntoParallelRefIterator, IntoParallelIterator};
 
+use rayon::iter::{ParallelIterator, IntoParallelIterator, IntoParallelRefMutIterator, IndexedParallelIterator, IntoParallelRefIterator};
 
 const ITER: usize = 16;
 
-struct Map<T: Copy, const W: usize, const H: usize>(Vec<T>);
+struct Map<const W: usize, const H: usize>(Vec<f32>);
 
-impl<T: Copy, const W: usize, const H: usize> Map<T, W, H> {
-	fn new(def: T) -> Self {
-		Self(vec![def; W * H])
+impl<const W: usize, const H: usize> Map<W, H> {
+	fn new() -> Self {
+		Self(vec![0.0; W * H])
 	}
-
-	fn from_raw(raw: Vec<T>) -> Self {
+	
+	fn from_raw(raw: Vec<f32>) -> Self {
 		Self(raw)
 	}
-
-	fn get(&self, x: usize, y: usize) -> T {
-		self.0[x + y * W]
+	
+	fn get(&self, x: usize, y: usize) -> f32 {
+		*self.0.get(x + y * W).unwrap_or(&0.0)
 	}
-
-	fn set(&mut self, x: usize, y: usize, val: T) {
-		self.0[x + y * W] = val;
+	
+	fn set(&mut self, x: usize, y: usize, val: f32) {
+		if let Some(cell) = self.0.get_mut(x + y * W) { *cell = val };
 	}
 }
 
@@ -27,108 +27,94 @@ fn xi<const W: usize>(i: usize) -> (usize, usize) {
 	(i % W, i / W)
 }
 
-fn getns<T : Copy, const W: usize, const H: usize>(arrx: &Map<T, W, H>, arry: &Map<T, W, H>, i: usize, j: usize) -> (T, T, T, T) {
-	(arrx.get(i + 1, j), arrx.get(i - 1, j), arry.get(i, j + 1), arry.get(i, j - 1))
+fn getns<const W: usize, const H: usize>(arrx: &Map<W, H>, arry: &Map<W, H>, i: usize, j: usize) -> (f32, f32, f32, f32) {
+	(
+		arrx.get(i + 1, j),
+		if i == 0 { 0.0 } else { arrx.get(i - 1, j) },
+		arry.get(i, j + 1),
+		if j == 0 { 0.0 } else { arry.get(i, j - 1) },
+	)
 }
 
-fn diffuse<const W: usize, const H: usize>(b: u8, x: &mut Map<f32, W, H>, x0: &Map<f32, W, H>, diff: f32, dt: f32) {
+fn diffuse<const W: usize, const H: usize>(b: u8, x: &mut Map<W, H>, x0: &Map<W, H>, diff: f32, dt: f32) {
 	let a = dt * diff * (W - 2) as f32 * (H - 2) as f32;
 	lin_solve(b, x, x0, a, 1.0 + 6.0 * a);
 }
 
-fn lin_solve<const W: usize, const H: usize>(b: u8, x: &mut Map<f32, W, H>, x0: &Map<f32, W, H>, a: f32, c: f32) {
+fn lin_solve<const W: usize, const H: usize>(b: u8, x: &mut Map<W, H>, x0: &Map<W, H>, a: f32, c: f32) {
 	for _ in 0..ITER {
-		for j in 1..(H - 1) {
-			for i in 1..(W - 1) {
-				let ns = getns(x, x, i, j);
-				let a1 = a * (ns.0 + ns.1 + ns.2 + ns.3);
-				x.set(i, j, (x0.get(i, j) + a1) / c);
-			}
-		}
-		// *x = Map::<f32, W, H>::from_raw((0..x.0.len()).into_par_iter().map(|i| {
-		// 	let (i, j) = xi::<W>(i);
-		// 	let ns = getns(x, x, i, j);
-		// 	let a1 = a * (ns.0 + ns.1 + ns.2 + ns.3);
-		// 	(x0.get(i, j) + a1) / c
-		// }).collect());
+		*x = Map::<W, H>::from_raw((0..x.0.len()).into_par_iter().map(|i| {
+			let (i, j) = xi::<W>(i);
+			let ns = getns(x, x, i, j);
+			let a1 = a * (ns.0 + ns.1 + ns.2 + ns.3);
+			(x0.get(i, j) + a1) / c
+		}).collect());
 		
 		set_bnd(b, x);
 	}
 }
 
-fn project<const W: usize, const H: usize>(vx: &mut Map<f32, W, H>, vy: &mut Map<f32, W, H>, p: &mut Map<f32, W, H>, div: &mut Map<f32, W, H>) {
-	for j in 1..(H - 1) {
-		for i in 1..(W - 1) {
-			let ns = getns(vx, vy, i, j);
-			div.set(i, j, (ns.0 - ns.1 + ns.2 - ns.3) * -0.5 / H as f32);
-			p.set(i, j, 0.0);
-		}
-	}
-
+fn project<const W: usize, const H: usize>(vx: &mut Map<W, H>, vy: &mut Map<W, H>, p: &mut Map<W, H>, div: &mut Map<W, H>) {
+	div.0.par_iter_mut().zip(p.0.par_iter_mut()).enumerate().for_each(|(i, (div_cell, p_cell))| {
+		let (i, j) = xi::<W>(i);
+		let ns = getns(vx, vy, i, j);
+		*div_cell = (ns.0 - ns.1 + ns.2 - ns.3) * -0.5 / H as f32;
+		*p_cell = 0.0;
+	});
+	
 	set_bnd(0, div);
 	set_bnd(0, p);
 	lin_solve(0, p, div, 1.0, 6.0);
-
-	for j in 1..(H - 1) {
-		for i in 1..(W - 1) {
-			let ns = getns(p, p, i, j);
-			vx.set(i, j, vx.get(i, j) - 0.5 * (ns.0 - ns.1) * W as f32);
-			vy.set(i, j, vy.get(i, j) - 0.5 * (ns.2 - ns.3) * H as f32);
-		}
-	}
-
+	
+	let (nvx, nvy) = vx.0.par_iter().zip(vy.0.par_iter()).enumerate().map(|(i, (vx_cell, vy_cell))| {
+		let (i, j) = xi::<W>(i);
+		let ns = getns(p, p, i, j);
+		(vx_cell - 0.5 * (ns.0 - ns.1) * W as f32, vy_cell - 0.5 * (ns.2 - ns.3) * H as f32)
+	}).unzip();
+	
+	*vx = Map::from_raw(nvx);
+	*vy = Map::from_raw(nvy);
+	
 	set_bnd(1, vx);
 	set_bnd(2, vy);
 }
 
-fn advect<const W: usize, const H: usize>(b: u8, d: &mut Map<f32, W, H>, d0: &Map<f32, W, H>, vx: &Map<f32, W, H>, vy: &Map<f32, W, H>, dt: f32) {
+fn advect<const W: usize, const H: usize>(b: u8, d: &mut Map<W, H>, d0: &Map<W, H>, vx: &Map<W, H>, vy: &Map<W, H>, dt: f32) {
 	let dtx = dt * (W - 2) as f32;
 	let dty = dt * (H - 2) as f32;
-
-	for j in 1..(H - 1) {
-		for i in 1..(W - 1) {
-			let tmpx = dtx * vx.get(i, j);
-			let tmpy = dty * vy.get(i, j);
-
-			let x = (i as f32 - tmpx).clamp(0.5, W as f32 + 0.5);
-			let i0 = x.floor() as usize;
-			let i1 = i0 + 1;
-
-			let y = (j as f32 - tmpy).clamp(0.5, H as f32 + 0.5);
-			let j0 = y.floor() as usize;
-			let j1 = j0 + 1;
-
-			let s1 = x - i0 as f32;
-			let s0 = 1.0 - s1;
-			let t1 = y - j0 as f32;
-			let t0 = 1.0 - t1;
-
-			let i0i = (i0 as usize).clamp(0, W - 1);
-			let i1i = (i1 as usize).clamp(0, W - 1);
-			let j0i = (j0 as usize).clamp(0, H - 1);
-			let j1i = (j1 as usize).clamp(0, H - 1);
-
-			d.set(i, j,
-				s0 * (t0 * d0.get(i0i, j0i) + t1 * d0.get(i0i, j1i)) +
-				s1 * (t0 * d0.get(i1i, j0i) + t1 * d0.get(i1i, j1i))
-			);
-		}
-	}
-
+	
+	d.0.par_iter_mut().enumerate()
+	.map(|(i, cell)| (xi::<W>(i), cell))
+	.filter(|&((i, j), _)| i != 1 && j != 1 && i != W - 1 && j != W - 1)
+	.for_each(|((i, j), cell)| {
+		let x = (i as f32 - dtx * vx.get(i, j)).clamp(0.5, W as f32 + 0.5);
+		let y = (j as f32 - dty * vy.get(i, j)).clamp(0.5, H as f32 + 0.5);
+		
+		let i0 = x.floor() as usize;
+		let j0 = y.floor() as usize;
+		
+		let s1 = x - i0 as f32;
+		let t1 = y - j0 as f32;
+		
+		*cell =
+		(1.0 - s1) * ((1.0 - t1) * d0.get(i0    , j0) + t1 * d0.get(i0    , j0 + 1)) +
+		s1         * ((1.0 - t1) * d0.get(i0 + 1, j0) + t1 * d0.get(i0 + 1, j0 + 1));
+	});
+	
 	set_bnd(b, d);
 }
 
-fn set_bnd<const W: usize, const H: usize>(b: u8, x: &mut Map<f32, W, H>) {
+fn set_bnd<const W: usize, const H: usize>(b: u8, x: &mut Map<W, H>) {
 	for i in 1..(W - 1) {
 		x.set(i, 0, if b == 2 { -1.0 } else { 1.0 } * x.get(i, 1));
 		x.set(i, H - 1, if b == 2 { -1.0 } else { 1.0 } * x.get(i, H - 2));
 	}
-
+	
 	for j in 1..(H - 1) {
 		x.set(0, j, if b == 1 { -1.0 } else { 1.0 } * x.get(1, j));
 		x.set(W - 1, j, if b == 1 { -1.0 } else { 1.0 } * x.get(W - 2, j));
 	}
-
+	
 	x.set(0, 0, 0.5 * (x.get(1, 0) + x.get(0, 1)));
 	x.set(0, H - 1, 0.5 * (x.get(1, H - 1) + x.get(0, H - 2)));
 	x.set(W - 1, 0, 0.5 * (x.get(W - 2, 0) + x.get(W - 1, 1)));
@@ -139,12 +125,12 @@ pub struct Fluid<const W: usize, const H: usize> {
 	dt: f32,
 	diff: f32,
 	visc: f32,
-	s: Map<f32, W, H>,
-	density: Map<f32, W, H>,
-	vx: Map<f32, W, H>,
-	vy: Map<f32, W, H>,
-	vx0: Map<f32, W, H>,
-	vy0: Map<f32, W, H>,
+	s: Map<W, H>,
+	density: Map<W, H>,
+	vx: Map<W, H>,
+	vy: Map<W, H>,
+	vx0: Map<W, H>,
+	vy0: Map<W, H>,
 }
 
 impl<const W: usize, const H: usize> Fluid<W, H> {
@@ -153,29 +139,29 @@ impl<const W: usize, const H: usize> Fluid<W, H> {
 			dt,
 			diff,
 			visc,
-			s: Map::new(0.0),
-			density: Map::new(0.0),
-			vx: Map::new(0.0),
-			vy: Map::new(0.0),
-			vx0: Map::new(0.0),
-			vy0: Map::new(0.0),
+			s: Map::new(),
+			density: Map::new(),
+			vx: Map::new(),
+			vy: Map::new(),
+			vx0: Map::new(),
+			vy0: Map::new(),
 		}
 	}
-
+	
 	pub fn step(&mut self) {
 		diffuse(1, &mut self.vx0, &self.vx, self.visc, self.dt);
 		diffuse(2, &mut self.vy0, &self.vy, self.visc, self.dt);
-
+		
 		project(&mut self.vx0, &mut self.vy0, &mut self.vx, &mut self.vy);
-
+		
 		advect(1, &mut self.vx, &self.vx0, &self.vx0, &self.vy0, self.dt);
 		advect(2, &mut self.vy, &self.vy0, &self.vx0, &self.vy0, self.dt);
-
+		
 		project(&mut self.vx, &mut self.vy, &mut self.vx0, &mut self.vy0);
 		diffuse(0, &mut self.s, &self.density, self.diff, self.dt);
 		advect(0, &mut self.density, &mut self.s, &self.vx, &self.vy, self.dt);
 	}
-
+	
 	pub fn add_density(&mut self, x: usize, y: usize, amount: f32) {
 		if x >= W || y >= H { return; }
 		self.density.set(x, y, (self.density.get(x, y) + amount).clamp(0.0, 255.0));
@@ -186,12 +172,12 @@ impl<const W: usize, const H: usize> Fluid<W, H> {
 		self.vx.set(x, y, self.vx.get(x, y) + ax);
 		self.vy.set(x, y, self.vy.get(x, y) + ay);
 	}
-
+	
 	#[allow(dead_code)]
 	pub fn get_density_at(&self, x: usize, y: usize) -> f32 {
 		self.density.get(x, y)
 	}
-
+	
 	#[allow(dead_code)]
 	pub fn get_vel_at(&self, x: usize, y: usize) -> (f32, f32) {
 		(self.vx.get(x, y), self.vy.get(x, y))
--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,31 @@ mod fluid;
 use fluid::Fluid;
 use nannou::{prelude::*, image::{ImageBuffer, Rgb, DynamicImage}, wgpu::Texture};
 
-const WIDTH: usize = 80;
-const HEIGHT: usize = 80;
+const WIDTH: usize = 128;
+const HEIGHT: usize = 128;
 
-const DT: f32 = 0.05;
-const DIFF: f32 = 1e-4;
+const DT: f32 = 0.1;
+const DIFF: f32 = 1e-6;
 const VISC: f32 = 1e-8;
 
-const FM: f32 = 0.2;
+const FM: f32 = 0.18;
+
+const BRUSH_RAD: isize = 8;
+const BRUSH_STRENGTH: f32 = 255.0;
 
 struct Model {
-	time: f32,
 	paused: bool,
-	pressing: bool,
+	left_pressed: bool,
+	right_pressed: bool,
 	prev_mouse: Point2,
 	fluid: Fluid<WIDTH, HEIGHT>,
 }
 
 fn setup(_app: &App) -> Model {
 	Model {
-		time: 0.0,
 		paused: false,
-		pressing: false,
+		left_pressed: false,
+		right_pressed: false,
 		prev_mouse: pt2(0.0, 0.0),
 		fluid: Fluid::new(DT, DIFF, VISC),
 	}
@@ -37,29 +40,22 @@ fn update(app: &App, model: &mut Model, event: Event) {
 			match event {
 				KeyPressed(Key::Space) => model.paused = !model.paused,
 				KeyPressed(Key::R) => *model = setup(app),
-				MousePressed(_) => model.pressing = true,
-				MouseReleased(_) => model.pressing = false,
+				MousePressed(MouseButton::Left) => model.left_pressed = true,
+				MouseReleased(MouseButton::Left) => model.left_pressed = false,
+				MousePressed(MouseButton::Right) => model.right_pressed = true,
+				MouseReleased(MouseButton::Right) => model.right_pressed = false,
 				MouseMoved(p) => {
 					let prev_mouse = model.prev_mouse;
 					model.prev_mouse = p;
 
-					if model.pressing && !model.paused {
+					if model.right_pressed || model.left_pressed {
 						let mdx = p.x - prev_mouse.x;
 						let mdy = p.y - prev_mouse.y;
 
 						let win = app.window_rect();
 
-						let cx = map_range(p.x, win.left(), win.right(), 0, WIDTH) as usize;
-						let cy = map_range(p.y, win.top(), win.bottom(), 0, HEIGHT) as usize;
-
-						const R: isize = 3;
-
-						for x in -R..=R {
-							let y_max = ((R as f32).pow(2.0) - (x as f32).pow(2.0)).sqrt() as usize;
-							for y in (cy - y_max)..=(cy + y_max) {
-								model.fluid.add_density((cx as isize + x) as usize, y, 255.0);
-							}
-						}
+						let cx = map_range(model.prev_mouse.x.clamp(win.left(), win.right()), win.left(), win.right(), 0, WIDTH) as usize;
+						let cy = map_range(model.prev_mouse.y.clamp(win.bottom(), win.top()), win.top(), win.bottom(), 0, HEIGHT) as usize;
 
 						model.fluid.add_velocity(cx as usize, cy as usize, mdx * FM, -mdy * FM);
 					}
@@ -67,9 +63,27 @@ fn update(app: &App, model: &mut Model, event: Event) {
 				_ => {}
 			}
 		}
-		Event::Update(update) if !model.paused => {
-			model.time += update.since_last.as_secs_f32();
-			model.fluid.step();
+		Event::Update(_) => {
+
+			if model.left_pressed {
+				let win = app.window_rect();
+	
+				let cx = map_range(model.prev_mouse.x, win.left(), win.right(), 0, WIDTH) as usize;
+				let cy = map_range(model.prev_mouse.y, win.top(), win.bottom(), 0, HEIGHT) as usize;
+	
+				for x in -BRUSH_RAD..=BRUSH_RAD {
+					let y_max = ((BRUSH_RAD as f32).pow(2.0) - (x as f32).pow(2.0)).sqrt() as isize;
+					for y in -y_max..=y_max {
+						// let d = 1.0 - (((x as f32).pow(2.0) + (y as f32).pow(2.0)).sqrt() / 3.0).pow(8.0);
+						let x = (cx as isize + x).clamp(0, isize::MAX) as usize;
+						let y = (cy as isize + y).clamp(0, isize::MAX) as usize;
+						model.fluid.add_density(x, y, BRUSH_STRENGTH);
+					}
+				}
+			}
+
+			if !model.paused { model.fluid.step(); }
+
 		}
 		_ => {}
 	}
